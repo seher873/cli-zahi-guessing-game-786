3af4aeeaf041028f201a3690c135cf9d7c0ca1e6
seher873 <161340426+seher873@users.noreply.github.com>
2024-04-24 21:25:43 -0700

    initial commit
    Guessing game

---
+++ main.ts
@@ -0,0 +1,19 @@
+#! /user/bin/env node
+import inquirer from "inquirer";
+//1) computer will genrate a rendum number = done
+//2) user input for guessing number= done
+//3) compair user input with computer gentaited number and show result
+const rendomNumber = Math.floor(Math.random( ) * 6 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess a number between 1-6:",
+    },
+]);
+if (answers.userGuessedNumber === rendomNumber) {
+    console.log("congtratulation! you guessed right number.");
+}
+else {
+    console.log("you guessed worng number");
+}
